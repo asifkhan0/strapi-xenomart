efeacbc2a38d7f74967251f6bc615572bb90439c
asifkhan0 <ashifkhan321.ak8@gmail.com>
2024-04-22 17:24:06 +0530

    Revert "Update webpack.config.js"
    This reverts commit d629dd95e01634bc5a68b462327a1fe8abbccd56.

--- src/admin/webpack.config.js
+++ src/admin/webpack.config.js
@@ -1,9 +1,16 @@
-'use strict';
-
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
 module.exports = (config) => {
-    config.plugins.push(new MonacoWebpackPlugin());
+    // Define your desired options for the plugin
+    const monacoOptions = {
+        languages: ['javascript', 'typescript'], // Include specific languages
+    };
+
+    try {
+        config.plugins.push(new MonacoWebpackPlugin(monacoOptions));
+    } catch (error) {
+        console.error('Error creating MonacoWebpackPlugin:', error);
+    }
 
     return config;
 };
