7b564cd765129e84a09c127fcdf05fcb53b0d638
asifkhan0 <ashifkhan321.ak8@gmail.com>
2024-04-22 16:45:30 +0530

    g

--- config/plugins.js
+++ config/plugins.js
@@ -5,8 +5,7 @@ module.exports = ({ env }) => ({
             providerOptions: {
                 cloud_name: env('CLOUDINARY_NAME'),
                 api_key: env('CLOUDINARY_KEY'),
-                api_secret: env('CLOUDINARY_SECRET'),
-                jwtSecret: env('JWT_SECRET')
+                api_secret: env('CLOUDINARY_SECRET')
             },
             actionOptions: {
                 upload: {},
