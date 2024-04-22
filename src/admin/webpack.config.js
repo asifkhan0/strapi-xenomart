d629dd95e01634bc5a68b462327a1fe8abbccd56
asifkhan0 <ashifkhan321.ak8@gmail.com>
2024-04-22 17:17:52 +0530

    Update webpack.config.js

--- src/admin/webpack.config.js
+++ src/admin/webpack.config.js
@@ -1,16 +1,9 @@
+'use strict';
+
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
 module.exports = (config) => {
-    // Define your desired options for the plugin
-    const monacoOptions = {
-        languages: ['javascript', 'typescript'], // Include specific languages
-    };
-
-    try {
-        config.plugins.push(new MonacoWebpackPlugin(monacoOptions));
-    } catch (error) {
-        console.error('Error creating MonacoWebpackPlugin:', error);
-    }
+    config.plugins.push(new MonacoWebpackPlugin());
 
     return config;
 };
